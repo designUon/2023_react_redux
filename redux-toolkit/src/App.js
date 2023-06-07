cbb6fbae7fdd41f21f59d4cee82bf9f8962ecebb
designUon <design_uon@naver.com>
2023-06-07 10:54:40 +0900

    리덕스 사용 설명 추가

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -8,9 +8,13 @@ import WeatherComp from './components/WeatherComp';
 function App() {
   return (
     <div className="App">
+      {/* 리덕스 툴킷 사용방법 */}
       <CounterBox/>
+      {/* 리덕스 툴킷에서 배열값 사용방법 */}
       <MemoComp/>
+      {/* 리덕스에서 비동기함수 사용 */}
       <ThunkBox/>
+      {/* 리덕스에서 비동기함수 활용하여 API 사용 */}
       <WeatherComp/>
     </div>
   );
